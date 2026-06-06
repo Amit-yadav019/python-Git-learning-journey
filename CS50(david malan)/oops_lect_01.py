d9bac2ccbfd86a0e362784aa3ac05dd4d29645c2
Amit Kumar <your-email@example.com>
2026-06-06 23:02:19 +0530

    new reprosetry 1st command commit

---
+++ CS50(david malan)/oops_lect_01.py
@@ -0,0 +1,17 @@
+class employee :
+    def __init__(self,first,last,pay,house):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.last = last
+        self.house = house
+        self.emai = first + "." + last + "@gmai.com"
+
+    def fullname(self):
+        return"{}{}".format(self.first,self.last)
+
+emp_1 = employee("corey","chase","50000","jonasburg")
+emp_2 = employee("glenn","maxwell","70000","sydney")
+
+print(emp_2.pay)
+
