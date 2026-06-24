b0ece66b8c70c319f161298d2420fff736f1311c
Amit Kumar <2024ugpi019@nitjsr.ac.in>
2026-06-24 23:12:51 +0530

    completed essence of algebra

--- CS50(david malan)/oops_lect_01.py
+++ CS50(david malan)/oops_lect_01.py
@@ -14,8 +14,12 @@ class employee :
 
 emp_1 = employee("corey","chase","50000","jonasburg")
 emp_2 = employee("glenn","maxwell","70000","sydney")
+emp_3 = employee("David","warner","75000","melborne")
+emp_4 = employee("sam","curran","89000","london")
 
 print(f"the salary of employee 2 is ",emp_2.pay)
 print(f"the salary of employee 1 is" ,emp_1.pay)
+print(f"the salary of employee 2 is ",emp_3.pay)
+print(f"the salary of employee 1 is" ,emp_4.pay)
 
 
